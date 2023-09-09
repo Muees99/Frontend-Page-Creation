164a7738af94a2fcd039a9998a2012afc6cb9977
Muees99 <132849693+Muees99@users.noreply.github.com>
2023-09-09 03:04:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ document.getElementById("CurrentDay").innerHTML = day;
 
 
 // To get Current Time
-const CurrentUTCtime = new Date();
+const CurrentUTCtime = new Date(currentDate);
 let time = CurrentUTCtime.getTime();
-document.getElementById("CurrentUTCtime").innerHTML = time;
+let hours=CurrentUTCtime.getUTCHours();
+const minutes=CurrentUTCtime.getUTCMinutes();
+const Currenttime=`${hours}:${minutes<10?"0" : ""}${minutes}`;
+
+document.getElementById("CurrentUTCtime").innerHTML = Currenttime;
